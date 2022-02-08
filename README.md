bbd2e7f9e03ce6197db31cde7f5a920ae7e204fc
kakaemma <7983999+kakaemma@users.noreply.github.com>
2022-02-08 16:13:50 +0300

    feat(decoding): decoding given string (#2)
    - add implementation to make the tests pass validation
    - add implementation to decode string
    [#181166422]

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# OrtusAfricaCapitalChallenge
+# Decode String
+## Running the challenge
+- Clone the project with `git clone https://github.com/kakaemma/DecodeString.git`
+- switch to the project folder
+- in your terminal, stwich to the project folder as well
+- open challenge.rb file with your preferred IDE or editor
+- change econded_text value to to a valid encoded string
+- go to your turn and run ruby challenge.rb
+- a decoded string should be printed
+
+## runing tests
+- run bundle exec rspec
+
--- challenge.rb
+++ challenge.rb
@@ -1,12 +1,42 @@
 class Challenge
-  def self.decode_string(encoded_str)
-    data_validation(encoded_str)
-  end
-
-  def self.data_validation(encoded_data)
-    if(encoded_data.class!=String)
-        'Encoded data should be a string'
+    def self.decode_data(encoded_str)
+        data = encoded_str.split('')
+        counter = data.count(']')
+        if counter==1
+            return decode_single_br(encoded_str)
+        end
+        if counter >1
+            count =0
+            decrypt_container = []
+            counter.times do
+                if count==0
+                    decrypt_txt = decode_single_br(encoded_str)
+                    decrypt_container.push(decrypt_txt.to_s)
+                elsif count>0
+                    new_string = decrypt_container.pop()
+                    decrypt_txt = decode_single_br(new_string)
+                    decrypt_container.push(decrypt_txt)
+                end
+                count+=1
+            end
+            return decrypt_container.pop
+        end
     end
-  end
+
+    def self.decode_single_br (encoded_data)
+        data = encoded_data.split('')
+        opening= data.index('[')
+        closing= data.index(']')
+        multiplier = data[opening-1].to_i
+        rpt_string = data[opening+1..closing-1].join
+        str_repeated = rpt_string*multiplier
+        remaining_string = ''+data[closing+1..data.length].join
+        str_data = data[0..opening-2].join
+        new_string = str_data+str_repeated+remaining_string
+        return new_string
+    end
 
 end
+
+ecoded_text = 'a3[b]3[d]4[o]'
+puts Challenge.decode_data(ecoded_text)
